d8b86bc53105d5772b73c700d9ee77023ff83741
Qamynn <ruijsromek@gmail.com>
2021-09-09 20:37:37 +0200

    Removed tuple() from PaletteSwap(), since it wasn't necessary

--- pgeng/Colour.py
+++ pgeng/Colour.py
@@ -10,8 +10,6 @@ def PaletteSwap(Surface, Colours):
 	Colours = {OldColour: NewColour, OldColour: NewColour}
 
 	Returns: pygame.Surface'''
-	for Colour in Colours:
-		Colours[Colour] = tuple(Colours[Colour])
 	NewSurface = Surface.copy()
 	for Y in range(NewSurface.get_height()):
 		for X in range(NewSurface.get_width()):
