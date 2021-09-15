0e3f21725fa4e38e312215c57d4fc6c217bb5cb7
root <root@RomekIdeaPad.localdomain>
2021-09-15 14:15:01 +0200

    Added FailedImports to __init__.py

--- pgeng/__init__.py
+++ pgeng/__init__.py
@@ -1,61 +1,67 @@
 '''Useful functions and classes for PyGame'''
+#VARIABLES
+FailedImports = []
+#VARIABLES
+
 #COLOR
 try:
 	from .Colour import *
-except ImportError:
-	pass
+except ImportError as e:
+	FailedImports.append(e.name)
 #COLOR
 
 #CORE
 try:
 	from .Core import *
-except ImportError:
-	pass
+except ImportError as e:
+	FailedImports.append(e.name)
 #CORE
 
 #ENTITY
 try:
 	from .Entity import Entity
-except ImportError:
-	pass
+except ImportError as e:
+	FailedImports.append(e.name)
 #ENTITY
 
 #FONT
 try:
 	from .Font import *
-except ImportError:
-	pass
+except ImportError as e:
+	FailedImports.append(e.name)
 #FONT
 
 #SCREEN
 try:
 	from .Screen import Screen
-except ImportError:
-	pass
+except ImportError as e:
+	FailedImports.append(e.name)
 #SCREEN
 
 #TILE
 try:
 	from .Tile import *
-except ImportError:
-	pass
+except ImportError as e:
+	print(type(e.name))
+	FailedImports.append(e.name)
 #TILE
 
 #SOUNDS
 try:
 	from .Sounds import Sounds
-except ImportError:
-	pass
+except ImportError as e:
+	FailedImports.append(e.name)
 #SOUNDS
 
 #VISUALEFFECTS
 try:
 	from .VisualEffects import *
-except ImportError:
-	pass
+except ImportError as e:
+	FailedImports.append(e.name)
 #VISUALEFFECTS
 
-#MESSAGE
+#MESSAGES
 print('''\nDo help(pgeng.<Function>) to get the documentation of the function (or possibly class)
 Or do help(pgeng.<Package Content>) to get the documentation of the functions or classes in a file\n''')
-#MESSAGE
+print(f'Failed to import {", ".join([Import.split(".")[1] for Import in FailedImports])} from pgeng') if FailedImports else None
+#MESSAGES
