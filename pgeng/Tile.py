9b7ea2cb4ba7b6184ad26a1cf6823b6c0f6fa7c5
Qamynn <ruijsromek@gmail.com>
2021-10-13 18:10:51 +0200

    Size is a list with the width and height

--- pgeng/Tile.py
+++ pgeng/Tile.py
@@ -4,19 +4,21 @@ import pygame
 #IMPORTS
 
 #VARIABLES
-__all__ = ['tile_size', 'Tile']
 tile_size = 0
 #VARIABLES
 
 #TILE
 class Tile:
 	'''A tile used by the Entity object to check collision
+	size can be a single number and it will be the width and the height, or it can be a tuple or list
 	It also supports ramps (45 degrees)
-	Ramp 0 = No ramp
-	Ramp 1 = top left
-	Ramp 2 = top right
-	Ramp 3 = bottom right
-	Ramp 4 = bottom left
+	If the width and height are not the same, ramp will be 0
+	ramp 0 = no ramp
+	ramp 1 = top left
+	ramp 2 = top right
+	ramp 3 = bottom right
+	ramp 4 = bottom left
+
 
 	Attributes:
 
@@ -27,16 +29,16 @@ class Tile:
 	ramp'''
 	#__INIT__
 	def __init__(self, location, size, ramp=0):
-		'''Initialising a tile'''
+		'''Initialising a Tile object'''
 		self.location = list(location)
-		self.size = size
-		self.ramp = ramp
+		self.size = list(size) if type(size) is list or type(size) is tuple else [size, size]
+		self.ramp = ramp if self.size[0] == self.size[1] else 0
 	#__INIT__
 
 	#RECT
 	@property
 	def rect(self):
-		'''Returns a pygame.Rect object of the tile'''
-		return pygame.Rect(self.location[0], self.location[1], self.size, self.size)
+		'''Returns a pygame.Rect object of the Tile object'''
+		return pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1])
 	#RECT
 #TILE
