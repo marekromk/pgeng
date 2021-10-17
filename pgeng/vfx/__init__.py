be474bb6db0b5663f2161c336e342ceb479843b2
Qamynn <>
2021-10-17 13:12:11 +0200

    Fixed failed_imports so it gets deleted and is invisible

--- pgeng/vfx/__init__.py
+++ pgeng/vfx/__init__.py
@@ -1,39 +1,39 @@
 '''Visual effects classes and functions for pgeng'''
 #IMPORTS
-from .. import failed_imports
+from .. import failed_imports as _failed_imports #UNDERSCORE SO IT IS NOT VISIBLE AS A VARIABLE
 #IMPORTS
 
 #CORE
 try:
     from .core import circle_lighting
 except ImportError as e:
-    failed_imports.append(e.name)
+    _failed_imports.append(e.name)
 #CORE
 
 #FLAME
 try:
     from .flame import *
 except ImportError as e:
-    failed_imports.append(e.name)
+    _failed_imports.append(e.name)
 #FLAME
 
 #PARTICLE
 try:
     from .particle import Particle
 except ImportError as e:
-    failed_imports.append(e.name)
+    _failed_imports.append(e.name)
 #PARTICLE
 
 #SHOCKWAVE
 try:
     from .shockwave import ShockWave
 except ImportError as e:
-    failed_imports.append(e.name)
+    _failed_imports.append(e.name)
 #SHOCKWAVE
 
 #SPARK
 try:
     from .spark import Spark
 except ImportError as e:
-    failed_imports.append(e.name)
+    _failed_imports.append(e.name)
 #SPARK
