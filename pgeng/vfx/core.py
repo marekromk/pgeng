f9daa10bb6fd001e56d3369235940c06f538b30e
Qamynn <ruijsromek@gmail.com>
2023-04-21 18:03:39 +0200

    Added support for the colour black in circle_lighting and added documentation in the code

--- pgeng/vfx/core.py
+++ pgeng/vfx/core.py
@@ -7,9 +7,12 @@ def circle_lighting(radius, colour, alpha=255):
 	The surface can have an alpha
 
 	Returns: pygame.Surface'''
-	lighting_surface = pygame.Surface((int(radius) * 2, int(radius) * 2))
-	lighting_surface.set_colorkey((0, 0, 0))
+	tuple_colour = tuple(colour[:3])
+	surface = pygame.Surface((int(radius) * 2, int(radius) * 2))
+	surface.set_colorkey((0, 0, 0) if tuple_colour != (0, 0, 0) else (1, 0, 0)) #there should be no background
+	if tuple_colour == (0, 0, 0):
+		surface.fill((1, 0, 0))
 	if alpha != 255:
-		lighting_surface.set_alpha(alpha)
-	pygame.draw.circle(lighting_surface, colour, (radius, radius), radius)
-	return lighting_surface
+		surface.set_alpha(alpha)
+	pygame.draw.circle(surface, colour, (radius, radius), radius)
+	return surface
